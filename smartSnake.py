c699da4bfe6fe81115f3974357dfb600b9944ad2
Anas Al Badi <anas.albadi@bmgroup.com>
2024-05-28 17:19:42 -0500

    ADD setting screen and reseting game

--- smartSnake.py
+++ smartSnake.py
@@ -60,3 +60,23 @@ def _update_ui(self):
     pygame.display.flip()
 
 #####################################################
+# Setting up the dimensions of the screen and the state of the game
+
+def __init__(self, w=640, h=480):
+    self.w = w
+    self.h = h
+    self.display = pygame.display.set_mode((self.w, self.h))
+    pygame.display.set_caption('Snake')
+    self.clock = pygame.time.Clock()
+
+def reset(self):
+    self.direction = Direction.RIGHT
+    self.head = Point(self.w / 2, self.h /2)
+    self.snake = [self.head, 
+                  Point(self.head.x - BLOCK_SIZE, self.head.y),
+                  Point(self.head.x - (2 * BLOCK_SIZE), self.head.y)]
+    self.score = 0
+    self.food = None
+    self._place_food()
+    self.frame_iteration = 0
+
