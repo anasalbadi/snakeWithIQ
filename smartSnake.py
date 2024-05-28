1696485d25a822e22df8d748b3e89c0d44558eb5
Anas Al Badi <anas.albadi@bmgroup.com>
2024-05-28 16:54:54 -0500

    ADD class for points

--- smartSnake.py
+++ smartSnake.py
@@ -27,3 +27,31 @@ class Direction(Enum):
         return f"Direction: {self.name}, Value: {self.value}"
 # print(Direction.RIGHT.describe())
 
+#####################################################
+# Create a class named Point for x and y
+Point = namedtuple('Point', 'x, y')
+
+#####################################################
+
+WHITE = (255, 255, 255)
+RED =   (200, 0, 0)
+BLUE1 = (0, 0, 255)
+BLUE2 = (0, 100, 255)
+BLACK = (0, 0, 0)
+
+BLOCK_SIZE = 20
+SPEED = 80 # Adjust the speed of teh snake to your liking
+
+#####################################################
+def _update_ui(self):
+    self.display.fill(BLACK)
+    for point in self.snake:
+        pygame.draw.rect(self.display, BLUE1, pygame.Rect(point.x, point.y, BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(self.display, BLUE2, pygame.Rect(point.x + 4, point.y + 4, 12, 12))
+
+    pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
+
+    text = font.render("Score: " + str(self.score), True, WHITE)
+    self.display.blit(text, [0, 0])
+    pygame.display.flip()
+
